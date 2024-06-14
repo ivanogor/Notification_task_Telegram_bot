a8d001846ab3362683b3119c7be4d00d0e6cf3f4
ivanogor <ivanogor23@gmail.com>
2024-06-14 23:55:16 +0800

    In pom.xml change Java version to 21. In TelegramBotUpdatesListener create answer for "/start".

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String text = update.message().text();
+            long chatId = update.message().chat().id();
+            //will add more case for switch
+            switch (text) {
+                case "/start" -> telegramBot.execute(new SendMessage(chatId, "Привет, этот телеграмм бот поможет тебе создать напоминание."));
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
